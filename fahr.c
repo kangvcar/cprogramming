fef6de026c425710d5157bbc1a633f7c672ddb91
kangvcar <kangvcar@126.com>
2017-04-20 03:35:41 +0800

    version 3

--- fahr.c
+++ fahr.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 main()
 {
-	//int fahr, celsius;
-	float fahr, celsius;
-	int lower, upper, step;
-	
-	lower = 0;
-	upper = 300;
-	step = 20;
-	
-	fahr = lower;
-	while (fahr <= upper) {
-		celsius = 5.0/9.0 * (fahr - 32);
-		printf("%3.0f %6.1f\n", fahr, celsius);
-		fahr = fahr + step;
-	}
+	int fahr;
+
+	for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
