665d5dce9b8908f4838a98da1db422b59d2544c8
kangvcar <kangvcar@126.com>
2017-04-20 04:16:48 +0800

    version 5

--- getchar.c
+++ getchar.c
@@ -2,8 +2,10 @@
 
 main()
 {
-	double nc;
-	for (nc = 0; getchar() != EOF; ++nc)
-		;
-	printf("%.0f\n", nc);
+	int c, nl;
+	nl = 0;
+	while((c = getchar()) != EOF)
+		if (c == '\n')
+			++nl;
+	printf("%d\n", nl);
 }
