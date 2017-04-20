6ccc0a7273f28174579550400cd52e618a3434aa
kangvcar <kangvcar@126.com>
2017-04-20 19:07:30 +0800

    version 3 垂直直方图

--- 1-13.c
+++ 1-13.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 
-#define MAXHIST 15	//max length of histogram
-#define MAXWORD 11	//max length of a word
+#define MAXHIST	15	//max length of histogram
+#define MAXWORD	11	//max length of a word
 #define IN	1	//inside a word
 #define OUT	0	//outside a word
-/* print horizontal histogram */
+
+/* print vertical histogram */
 main()
 {
-	int c, i, nc, state;
-	int len;	//length of each bar
+	int c, i, j, nc, state;
 	int maxvalue;	//maximum value for wl[]
 	int ovflow;	//number of overflow words
 	int wl[MAXWORD];	//word length counters
-
 	state = OUT;
 	nc = 0;		//number of chars in a word
 	ovflow = 0;	//number of words >= MAXWORD
-	for(i = 0; i < MAXWORD; ++i){
-		wl[i] = 0;	//用0填满数组
-	}
-	while((c = getchar()) != EOF){
-		if(c == ' ' || c == '\n' || c == '\t'){
+	for(i=0; i<MAXWORD; ++i)
+		wl[i] = 0;
+	while((c = getchar()) != EOF) {
+		if(c == ' ' || c == '\n' || c == '\t') {
 			state = OUT;
 			if(nc > 0)
 				if(nc < MAXWORD)
@@ -30,28 +28,29 @@ main()
 			nc = 0;
 		}else if(state == OUT){
 			state = IN;
-			nc = 1;		//beginning of a new word
+			nc = 1;		//beginning of a word
 		}else
 			++nc;		//inside a word
 	}
 	maxvalue = 0;
-	for(i = 1; i < MAXWORD; ++i){
+	for(i=1; i<MAXWORD; ++i)
 		if(wl[i] > maxvalue)
 			maxvalue = wl[i];
-	}
-	for(i = 1; i < MAXWORD; ++i){
-		printf("%5d - %5d :", i, wl[i]);
-		if(wl[i] > 0){
-			if((len = wl[i]*MAXHIST/maxvalue) <= 0)
-				len = 1;
-		}else
-			len = 0;
-		while(len > 0){
-			putchar('*');
-			--len;
-		}
+
+	for(i=MAXHIST; i>0; --i){
+		for(j=1; j<MAXWORD; ++j)
+			if(wl[j]*MAXHIST/maxvalue >=i)
+				printf("   * ");
+			else
+				printf("     ");
 		putchar('\n');
 	}
+	for(i=1; i<MAXWORD; ++i)
+		printf("%4d ", i);
+	putchar('\n');
+	for(i=1; i<MAXWORD; ++i)
+		printf("%4d ", wl[i]);
+	putchar('\n');
 	if(ovflow > 0)
 		printf("There are %d words >= %d\n", ovflow, MAXWORD);
 }
